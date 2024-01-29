21b27f8f6ec8ce94c9bf04c1d2a7d44ea5ade442
Dylan <dylannguyen131@gmail.com>
2024-01-29 11:44:59 +0700

    Modify db function

--- graphdb.py
+++ graphdb.py
@@ -10,8 +10,29 @@ def init_driver(uri, username, password):
 
 
 
-def create_node(tx, name, relationship):
-    pass
+def db_create_node(tx, node:LineageNode):
+    cypher="""
+    CREATE (node:$node_type {name:'$name', file_name:'$file_name', id:$id})
+"""
+    name=node.name
+    file_name=node.file_name
+    id=node.id
+
+    return tx.run(cypher, name=name, file_name=file_name, id=id)
+
+def db_match_query_column(tx, node:LineageNode):
+    cypher= """
+    MATCH (query: $nodetype {file_name: '$file_name', id: $id, name:'$name'})
+    MATCH (column: 'Column' {file_name: '$file_name'}) WHERE column.id IN [$id_list]
+    CREATE (query) -[:Contain_Column] -> (column)
+"""
+    nodetype = node.type
+    file_name = node.file_name
+    id = node.id
+    id_list = ",".join([col.id for col in node.downstream])
+
+
+    return tx.run(cypher, file_name=file_name, nodetype=nodetype, id=id, id_list=id_list)
 
 def find_node():
     pass
@@ -22,4 +43,4 @@ def delete_node():
 
 driver = init_driver('','','')
 with driver.session() as session:
-    nodes = session.execute_write(create_node,name='NodeA', relationship='Contain')
+    nodes = session.execute_write(db_create_node,name='NodeA', relationship='Contain')
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from lineage import *
+from graphdb import *
 from pprint import pprint
 sql = """
                               
@@ -14,12 +15,13 @@ select col1, col2 from table2
 
 # print(repr(get_lineage(sql)))
 rootnode = get_lineage(sql, file_name='test1')
-pprint(rootnode)
+# pprint(rootnode)
 
 
 # print(rootnode.downstream[0])
 
 
+driver=init_driver(r"neo4j://localhost[:7687] ", "neo4j", "DataLineage")
 
-
+driver
 
