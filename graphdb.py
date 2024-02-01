952d3e9944a5af64e7c94359567341cd30f5c2e9
Dylan1301 <dylannguyen131@gmail.com>
2024-02-01 22:43:28 +0700

    Update graphdb file

--- graphdb.py
+++ graphdb.py
@@ -2,7 +2,7 @@ from lineage import *
 from neo4j import GraphDatabase
 
 def init_driver(uri, username, password):
-    driver = GraphDatabase.driver(uri, auth=(username,  password))
+    driver = GraphDatabase.driver(uri, auth=(username, password))
 
     driver.verify_connectivity
 
@@ -11,36 +11,37 @@ def init_driver(uri, username, password):
 
 
 def db_create_node(tx, node:LineageNode):
-    cypher="""
-    CREATE (node:$node_type {name:'$name', file_name:'$file_name', id:$id})
-"""
+    node_type= node.type
+    cypher=f"""CREATE (node: {node_type} {{name: $name, file_name: $file_name, id: $id}})"""
+
+    
     name=node.name
     file_name=node.file_name
     id=node.id
-
-    return tx.run(cypher, name=name, file_name=file_name, id=id)
+    
+    return tx.run(cypher, node_type=node_type, name=name, file_name=file_name, id=id)
 
 def db_match_query_column(tx, node:LineageNode):
-    cypher= """
-    MATCH (query: $nodetype {file_name: '$file_name', id: $id, name:'$name'})
-    MATCH (column: 'Column' {file_name: '$file_name'}) WHERE column.id IN [$id_list]
+
+    nodetype = node.type
+    cypher= f"""
+    MATCH (query: {nodetype} {{file_name: '$file_name', id: $id, name:'$name'}})
+    MATCH (column {{file_name: '$file_name'}}) WHERE column.id IN [$id_list]
     CREATE (query) -[:Contain_Column] -> (column)
 """
-    nodetype = node.type
     file_name = node.file_name
     id = node.id
-    id_list = ",".join([col.id for col in node.downstream])
+    id_list = ",".join([str(col.id) for col in node.downstream])
+    print(id_list)
 
+    return tx.run(cypher, file_name=file_name, id=id, id_list=id_list)
 
-    return tx.run(cypher, file_name=file_name, nodetype=nodetype, id=id, id_list=id_list)
-
-def find_node():
+def db_match_alias():
     pass
 
 def delete_node():
     pass
 
 
-driver = init_driver('','','')
-with driver.session() as session:
-    nodes = session.execute_write(db_create_node,name='NodeA', relationship='Contain')
+# with driver.session() as session:
+#     nodes = session.execute_write(db_create_node,name='NodeA', relationship='Contain')
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from lineage import *
 from graphdb import *
 from pprint import pprint
+from neo4j import GraphDatabase
 sql = """
                               
 
@@ -21,7 +22,23 @@ rootnode = get_lineage(sql, file_name='test1')
 # print(rootnode.downstream[0])
 
 
-driver=init_driver(r"neo4j://localhost[:7687] ", "neo4j", "DataLineage")
+# driver=init_driver("bolt://127.0.0.1:7687", "neo4j", "12345678")
 
-driver
+address="bolt://localhost:7687"
+auth=("neo4j", "12345678")
+driver = GraphDatabase.driver(address, auth=auth)
+driver.verify_authentication()
+
+
+# tx = driver.session()
+
+# db_create_node(tx, node=rootnode)
+
+with driver.session() as session:
+    
+    session.execute_write(db_create_node,node=rootnode)
+    for node in rootnode.downstream:
+        session.execute_write(db_create_node, node=node)
+    session.execute_write(db_match_query_column,node=rootnode)
+
 
